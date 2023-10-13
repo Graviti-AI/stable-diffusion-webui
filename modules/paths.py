83dde7d931f30411cc3f948a6affedcb413df38c
btian <ben.tian@graviti.cn>
2023-10-13 09:28:47 +0800

    save original image file to workdir instead if symlink for free user

--- modules/paths.py
+++ modules/paths.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import shutil
 import sys
 from modules.paths_internal import models_path, script_path, data_path, extensions_dir, extensions_builtin_dir, MODEL_CONTAINER_NAME  # noqa: F401
 
@@ -201,16 +202,10 @@ class Paths:
             # image is generated at public folder, make a symlink to src image
             src_path = pathlib.Path(filename)
             relative_to = src_path.relative_to(self._output_dir)
-            # out_put_dir = src_path.parents[3]
-            # dest_path = self._private_output_dir.joinpath(src_path.parts[-4],
-            #                                               src_path.parts[-3],
-            #                                               src_path.parts[-2],
-            #                                               src_path.parts[-1])
-            dest_path = self._private_output_dir.joinpath(relative_to)
-            if not dest_path.parent.exists():
-                dest_path.parent.mkdir(parents=True, exist_ok=True)
-            relative_to = os.path.relpath(src_path, dest_path.parent)
-            dest_path.symlink_to(relative_to)
+            dst_path = self._private_output_dir.joinpath(relative_to)
+            if not dst_path.parent.exists():
+                dst_path.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(src_path, dst_path)
 
 
 class Prioritize:

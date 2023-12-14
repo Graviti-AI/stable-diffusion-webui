e43a64978b7abe2ed31367ed726daecbdbf9c8e7
linji.xue <linji.xue@graviti.cn>
2023-12-14 22:45:39 +0800

    refactor: add downward compatibility for "sd_model.read_state_dict"

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -299,17 +299,24 @@ def read_metadata_from_safetensors(filename):
         return res
 
 
-def read_state_dict(model_info: ModelInfo, print_global_state=False, map_location=None):
-    if model_info.is_safetensors():
+def read_state_dict(model_info: ModelInfo | str, print_global_state=False, map_location=None):
+    if isinstance(model_info, ModelInfo):
+        filename = model_info.filename
+        is_safetensors = model_info.is_safetensors()
+    else:
+        filename = model_info
+        is_safetensors = os.path.splitext(filename)[-1] == ".safetensors"
+
+    if is_safetensors:
         device = map_location or shared.weight_load_location or devices.get_optimal_device_name()
 
         if not shared.opts.disable_mmap_load_safetensors:
-            pl_sd = safetensors.torch.load_file(model_info.filename, device=device)
+            pl_sd = safetensors.torch.load_file(filename, device=device)
         else:
-            pl_sd = safetensors.torch.load(open(model_info.filename, 'rb').read())
+            pl_sd = safetensors.torch.load(open(filename, 'rb').read())
             pl_sd = {k: v.to(device) for k, v in pl_sd.items()}
     else:
-        pl_sd = torch.load(model_info.filename, map_location=map_location or shared.weight_load_location)
+        pl_sd = torch.load(filename, map_location=map_location or shared.weight_load_location)
 
     if print_global_state and "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")

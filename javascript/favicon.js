088315a9b72c9b799793ddc7fcde28fcb77f6e03
linji.xue <linji.xue@graviti.com>
2024-01-23 15:30:42 +0800

    refactor: update the link of "favicon.ico"

--- javascript/favicon.js
+++ javascript/favicon.js
@@ -3,8 +3,7 @@ class FaviconHandler {
     const link = document.createElement('link');
     link.rel = 'icon';
     link.type = 'image/svg+xml';
-    link.href =
-      'https://images.squarespace-cdn.com/content/v1/6411cba29f7f4b4a7e9609c8/f86d5718-8b9c-444b-acd2-120472d8f931/favicon.ico?format=100w';
+    link.href = 'https://diffus-public-static-assets.s3.amazonaws.com/logos/diffus/favicon.ico';
     document.getElementsByTagName('head')[0].appendChild(link);
   }
 }

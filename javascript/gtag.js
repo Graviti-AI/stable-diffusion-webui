62822d99c40197823b2d8bafb5eb838c944b252c
Edward Cui <edward.cui@graviti.cn>
2024-01-25 22:29:18 +0800

    Push purchase info to ga

--- javascript/gtag.js
+++ javascript/gtag.js
@@ -19,3 +19,86 @@ function addGenerateGtagEvent(selector, itemName) {
         });
     }
 }
+
+function sendPurchaseEvent(callback = null) {
+    fetch(
+      '/api/payments/latest',
+      {
+        method: 'GET',
+        credentials: 'include',
+      }
+    )
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .then((data) => {
+        let promises = data.map(item => {
+            return new Promise((resolve, reject) => {
+                gtag("event", "purchase", {
+                    transaction_id: item.payment_id,
+                    value: item.payment_value,
+                    tax: item.tax,
+                    currency: "USD",
+                    items: [{
+                        item_id: item.product_id,
+                        item_name: item.product_name,
+                        discount: item.discount,
+                        item_category: item.product_category,
+                        price: item.price,
+                        quantity: item.quantity
+                    }],
+                    event_callback: function() {
+                        resolve();
+                    }
+                });
+            });
+        });
+
+        Promise.all(promises).then(() => {
+            if (typeof callback === 'function') {
+                callback();
+            }
+            const paymentIds = data.map(item => {
+              return {payment_id: item.payment_id}
+            });
+            fetch(
+              '/api/payments/reported',
+              {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(paymentIds)
+              }
+            )
+            .then(response => {
+              if (response.status === 200) {
+                return response.json();
+              }
+              return Promise.reject(response);
+            })
+            .catch((error) => {
+                console.error('update purchase info error:', error);
+            });
+        }).catch(error => {
+            console.error("sendPurchaseEvent error:", error);
+            if (typeof callback === 'function') {
+                callback();
+            }
+        });
+    })
+    .catch((error) => {
+        console.error('sendPurchaseEvent error:', error);
+        if (typeof callback === 'function') {
+            callback();
+        }
+    });
+}
+
+onUiLoaded(function(){
+    sendPurchaseEvent();
+});

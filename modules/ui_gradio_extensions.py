23546fbc797454e2b6febf1a7160d500e4c85edd
Edward Cui <edward.cui@graviti.cn>
2024-09-04 22:37:17 +0800

    Add intercom messenger to webui

--- modules/ui_gradio_extensions.py
+++ modules/ui_gradio_extensions.py
@@ -86,14 +86,28 @@ def css_html():
     return head
 
 
+def body_end_scripts():
+    body = ""
+    body += """
+    <script>
+    // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/idmms6yo'
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/idmms6yo';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    </script>
+    """
+
+    return body
+
+
 def reload_javascript():
     css = css_html()
 
     def template_response(*args, **kwargs):
         js = javascript_html(args[1]['request'])
+        body_end_scripts = body_end_scripts()
         res = shared.GradioTemplateResponseOriginal(*args, **kwargs)
         res.body = res.body.replace(b'</head>', f'{js}</head>'.encode("utf8"))
         res.body = res.body.replace(b'</body>', f'{css}</body>'.encode("utf8"))
+        res.body = res.body.replace(b'</body>', f'{body_end_scripts}</body>'.encode("utf8"))
         res.init_headers()
         return res
 

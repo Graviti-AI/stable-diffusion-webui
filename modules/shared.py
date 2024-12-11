59faf83e0a844764fc87995766db6c5656a709de
linji.xue <linji.xue@graviti.com>
2024-12-11 17:52:24 +0800

    refactor: remove old gallery related useless files and variables

--- modules/shared.py
+++ modules/shared.py
@@ -3,11 +3,11 @@ import sys
 
 import gradio as gr
 
-from modules import (
-    shared_items, util, shared_cmd_options, shared_gradio_themes, options, sd_models_types)
+from modules import shared_cmd_options, shared_gradio_themes, options, shared_items, sd_models_types
 import modules.styles
 from modules.paths_internal import models_path, script_path, data_path, sd_configs_path, sd_default_config, sd_model_file, default_sd_model_file, extensions_dir, extensions_builtin_dir  # noqa: F401
 from modules.paths import Paths
+from modules import util
 
 from typing import Optional
 

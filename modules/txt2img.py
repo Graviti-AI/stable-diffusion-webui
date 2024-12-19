e4d849cbb52221fc4fa8c7799791790138c44959
linji.xue <linji.xue@graviti.com>
2024-12-20 01:44:25 +0800

    fix: add the missing "restore_faces" in txt2img_create_processing back

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -18,7 +18,7 @@ import gradio as gr
 from modules_forge import main_thread
 
 
-def txt2img_create_processing(request: gr.Request, id_task: str, prompt: str, negative_prompt: str, prompt_styles, n_iter: int, batch_size: int, cfg_scale: float, distilled_cfg_scale: float, height: int, width: int, enable_hr: bool, denoising_strength: float, hr_scale: float, hr_upscaler: str, hr_second_pass_steps: int, hr_resize_x: int, hr_resize_y: int, hr_checkpoint_name: str, hr_additional_modules: list, hr_sampler_name: str, hr_scheduler: str, hr_prompt: str, hr_negative_prompt, hr_cfg: float, hr_distilled_cfg: float, override_settings_texts, *args, force_enable_hr=False):
+def txt2img_create_processing(request: gr.Request, id_task: str, prompt: str, negative_prompt: str, prompt_styles, n_iter: int, batch_size: int, cfg_scale: float, distilled_cfg_scale: float, height: int, width: int, enable_hr: bool, denoising_strength: float, hr_scale: float, hr_upscaler: str, hr_second_pass_steps: int, hr_resize_x: int, hr_resize_y: int, hr_checkpoint_name: str, hr_additional_modules: list, hr_sampler_name: str, hr_scheduler: str, hr_prompt: str, hr_negative_prompt, hr_cfg: float, hr_distilled_cfg: float, restore_faces: bool, override_settings_texts, *args, force_enable_hr=False):
     override_settings = create_override_settings_dict(override_settings_texts)
 
     hr_checkpoint_name = _get_str(hr_checkpoint_name)

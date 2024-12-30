f7533661e9f6708090e4d2f34b793b0f345fe263
linji.xue <linji.xue@graviti.com>
2024-12-30 21:32:14 +0800

    refactor: remove sd_hijack.model_hijack.comments.append in ScriptRunner

--- modules/scripts.py
+++ modules/scripts.py
@@ -890,9 +890,7 @@ class ScriptRunner:
             try:
                 script_args = p.script_args[script.args_from:script.args_to]
                 script.process(p, *script_args)
-            except Exception as e:
-                from modules import sd_hijack
-                sd_hijack.model_hijack.comments.append(f'Extension {script.title()} Process Error: {type(e).__name__}: {str(e)}')
+            except Exception:
                 errors.report(f"Error running process: {script.filename}", exc_info=True)
 
     def process_before_every_sampling(self, p, **kwargs):
@@ -932,9 +930,7 @@ class ScriptRunner:
             try:
                 script_args = p.script_args[script.args_from:script.args_to]
                 script.process_batch(p, *script_args, **kwargs)
-            except Exception as e:
-                from modules import sd_hijack
-                sd_hijack.model_hijack.comments.append(f'Extension {script.title()} Process Batch Error: {type(e).__name__}: {str(e)}')
+            except Exception:
                 errors.report(f"Error running process_batch: {script.filename}", exc_info=True)
 
     def process_before_every_sampling(self, p, **kwargs):
@@ -950,9 +946,7 @@ class ScriptRunner:
             try:
                 script_args = p.script_args[script.args_from:script.args_to]
                 script.postprocess(p, processed, *script_args)
-            except Exception as e:
-                from modules import sd_hijack
-                sd_hijack.model_hijack.comments.append(f'Extension {script.title()} Postprocess Error: {type(e).__name__}: {str(e)}')
+            except Exception:
                 errors.report(f"Error running postprocess: {script.filename}", exc_info=True)
 
     def postprocess_batch(self, p, images, **kwargs):
@@ -960,9 +954,7 @@ class ScriptRunner:
             try:
                 script_args = p.script_args[script.args_from:script.args_to]
                 script.postprocess_batch(p, *script_args, images=images, **kwargs)
-            except Exception as e:
-                from modules import sd_hijack
-                sd_hijack.model_hijack.comments.append(f'Extension {script.title()} Postprocess Batch Error: {type(e).__name__}: {str(e)}')
+            except Exception:
                 errors.report(f"Error running postprocess_batch: {script.filename}", exc_info=True)
 
     def postprocess_batch_list(self, p, pp: PostprocessBatchListArgs, **kwargs):
@@ -994,9 +986,7 @@ class ScriptRunner:
             try:
                 script_args = p.script_args[script.args_from:script.args_to]
                 script.postprocess_image(p, pp, *script_args)
-            except Exception as e:
-                from modules import sd_hijack
-                sd_hijack.model_hijack.comments.append(f'Extension {script.title()} Postprocess Image Error: {type(e).__name__}: {str(e)}')
+            except Exception:
                 errors.report(f"Error running postprocess_image: {script.filename}", exc_info=True)
 
     def postprocess_maskoverlay(self, p, ppmo: PostProcessMaskOverlayArgs):

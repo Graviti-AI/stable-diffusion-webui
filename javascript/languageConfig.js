40253439d98c2d687f83fb71830fbf659090fc24
linji.xue <linji.xue@graviti.com>
2025-04-18 18:11:53 +0800

    style: format "languageConfig.js" by "prettier"

--- javascript/languageConfig.js
+++ javascript/languageConfig.js
@@ -1,63 +1,61 @@
 const localization_name_mapper = {
-  None: 'English',
-  de_DE: 'Deutsch',
-  es_ES: 'Español',
-  fi_FI: 'Suomi',
-  fr_FR: 'Français',
-  it_IT: 'Italiano',
-  ja_JP: '日本語',
-  ko_KR: '한국어',
-  no_NO: 'Norwegian',
-  pt_BR: 'Português (Brasil)',
-  ru_RU: 'Pусский',
-  tr_TR: 'Türkçe',
-  zh_CN: '中文(简体)',
-  zh_TW: '中文(繁體)',
+    None: "English",
+    de_DE: "Deutsch",
+    es_ES: "Español",
+    fi_FI: "Suomi",
+    fr_FR: "Français",
+    it_IT: "Italiano",
+    ja_JP: "日本語",
+    ko_KR: "한국어",
+    no_NO: "Norwegian",
+    pt_BR: "Português (Brasil)",
+    ru_RU: "Pусский",
+    tr_TR: "Türkçe",
+    zh_CN: "中文(简体)",
+    zh_TW: "中文(繁體)",
 };
 
-const languageCookieKey = 'localization';
+const languageCookieKey = "localization";
 
 function setSelectChecked(selectId, checkValue) {
-  var select = gradioApp().querySelector(`#${selectId}`);
+    var select = gradioApp().querySelector(`#${selectId}`);
 
-  for (var i = 0; i < select.options.length; i++) {
-    if (select.options[i].value == checkValue) {
-      select.options[i].selected = true;
-      break;
+    for (var i = 0; i < select.options.length; i++) {
+        if (select.options[i].value == checkValue) {
+            select.options[i].selected = true;
+            break;
+        }
     }
-  }
 }
 
 function generateLanguageSelectOptions() {
-  const footerNode = gradioApp().querySelector(`#footer-nav`);
-  const languageListNode = gradioApp().querySelector(`#language-list`);
+    const footerNode = gradioApp().querySelector(`#footer-nav`);
+    const languageListNode = gradioApp().querySelector(`#language-list`);
 
-  const selectNode = document.createElement('select');
-  selectNode.classList = 'language-list';
-  selectNode.title = 'Select Language';
-  selectNode.id = 'language-select';
+    const selectNode = document.createElement("select");
+    selectNode.classList = "language-list";
+    selectNode.title = "Select Language";
+    selectNode.id = "language-select";
 
-  const laguageList = JSON.parse(
-    languageListNode.textContent.replaceAll("'", '"')
-  );
-  laguageList.forEach((language) => {
-    const optionNode = document.createElement('option');
-    optionNode.value = language;
-    optionNode.label = localization_name_mapper[language];
+    const laguageList = JSON.parse(languageListNode.textContent.replaceAll("'", '"'));
+    laguageList.forEach((language) => {
+        const optionNode = document.createElement("option");
+        optionNode.value = language;
+        optionNode.label = localization_name_mapper[language];
 
-    selectNode.appendChild(optionNode);
-  });
-  footerNode.appendChild(selectNode);
+        selectNode.appendChild(optionNode);
+    });
+    footerNode.appendChild(selectNode);
 }
 
 function adaptMobile() {
-  const footerNode = gradioApp().querySelector(`#footer-nav`);
-  const navList = footerNode.querySelectorAll('.nav-item');
-  const isMobile = window.innerWidth < 640;
-  isMobile && navList.forEach((nav) => nav.classList.remove('nav-item'));
+    const footerNode = gradioApp().querySelector(`#footer-nav`);
+    const navList = footerNode.querySelectorAll(".nav-item");
+    const isMobile = window.innerWidth < 640;
+    isMobile && navList.forEach((nav) => nav.classList.remove("nav-item"));
 }
 
 onUiLoaded(() => {
-  generateLanguageSelectOptions();
-  adaptMobile();
+    generateLanguageSelectOptions();
+    adaptMobile();
 });

51ed41b8be9f3871eb7b6afc585eed56ab41757f
linji.xue <linji.xue@graviti.com>
2025-05-09 14:06:09 +0800

    feat: add "NOOBAI" to "ModelInfo.base"

--- modules/model_info.py
+++ modules/model_info.py
@@ -93,7 +93,7 @@ class ModelInfoProtocal(Protocol):
 class ModelInfo(BaseModel):
     id: int
     model_type: Literal["CHECKPOINT", "EMBEDDING", "HYPERNETWORK", "LORA", "LYCORIS"]
-    base: Literal["SD1", "SD2", "SDXL", "PONY", "SD3", "FLUX", "ILLUSTRIOUS"] | None
+    base: Literal["SD1", "SD2", "SDXL", "PONY", "SD3", "FLUX", "NOOBAI", "ILLUSTRIOUS"] | None
     source: str | None = None
     name: str
     sha256: str
